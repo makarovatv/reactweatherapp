4dabd4ed7f5dc1f2591136c657d744df55a69ffb
makarovatv <makarovatv80@gmail.com>
2022-09-27 22:24:19 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="Weather App">
-      <h1>Weather</h1>
-      <footer>
-        The project was cooded by Tanya Makarova and is
-        <a href="https://github.com/makarovatv/reactweatherapp" target="_blank">
-          {" "}
-          open sourced on GitHub
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>Weather</h1>
+        <footer>
+          The project was cooded by Tanya Makarova and is
+          <a
+            href="https://github.com/makarovatv/reactweatherapp"
+            target="_blank"
+          >
+            {" "}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
