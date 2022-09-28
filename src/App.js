a0dce24e2a46a3994b06156e916a6481ba9e73fc
makarovatv <makarovatv80@gmail.com>
2022-09-28 12:49:10 +0300

    Added basic css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather</h1>
+        <Weather />
         <footer>
           The project was cooded by Tanya Makarova and is
           <a
