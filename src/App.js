32678a77e2d9b0d9de6c3bc63dd2ad4fc59c2447
makarovatv <makarovatv80@gmail.com>
2022-09-28 21:05:28 +0300

    Added API

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           The project was cooded by Tanya Makarova and is
           <a
