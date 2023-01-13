c9d55fc2952d5fcc3227afe5d25be351a467b3ab
makarovatv <makarovatv80@gmail.com>
2023-01-13 18:47:08 +0200

    refresh forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
+import userEvent from "@testing-library/user-event";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+     setLoaded(false);
+  }, [props.coordinates]);
+
 function handleResponse(response) {
   console.log(response.data);
   setForecast(response.data.daily);
